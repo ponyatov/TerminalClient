7d912ca0a733c57188aa2704c78ccb11b97339c7
Dmitry Ponyatov <dponyatov@gmail.com>
2024-02-27 19:30:42 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- inc/hello.hpp
+++ inc/hello.hpp
@@ -2,5 +2,17 @@
 
 #include <cstdio>
 
+/// @defgroup main main
+/// @{
+
+/// @brief print single cmd.line argument
+/// @param argc index
+/// @param argv value
 extern void arg(int argc, char argv[]);
+
+/// @brief executable entry point
+/// @param argc number of cmd.line arguments
+/// @param argv binary name + arguments
 extern int main(int argc, char *argv[]);
+
+/// @}
